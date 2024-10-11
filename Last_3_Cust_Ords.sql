3359d7baa69e81040a3bdc6be85feeec4f15665f
dhafdelin <daniel_hafdelin@outlook.com>
2024-10-10 21:35:30 -0700

    Add files via upload

--- Last_3_Cust_Ords.sql
+++ Last_3_Cust_Ords.sql
@@ -1,26 +1,22 @@
-
-
-  WITH CTE as
-  (
-  SELECT *,
-  ROW_NUMBER() OVER (PARTITION BY CUSTNAME
-					ORDER BY ORDERDATE DESC) as Ranking
-  FROM [OnlineRetailSales]
-  )
-  Select *
-  From CTE
-  where Ranking = 1
-
-
+--Obtain information on the last 3 customer orders
   with cte as
   (
-  Select OrderNum,OrderDate,CustName,ProdCategory,ProdName,Order_Total,
-  ROW_NUMBER() OVER (Partition By ProdCategory
+  Select OrderNum,OrderDate,CustName,Order_Total,
+  ROW_NUMBER() OVER (Partition By CustName
 						ORDER BY Order_Total Desc) Ranking
     FROM [OnlineRetailSales]
-	where Custname = 'Boehm Inc.'
 	)
 
-	Select * from CTE
-	Where Ranking in (1,2,3)
-  
+	Select OrderNum,OrderDate,CustName,Order_Total 
+	From CTE
+	Where Ranking in (1,2,3) and CustName In ('Abbot Group','Abernathy LLC','Abra Lewis')
+	Order by CustName,OrderDate
+/*  
+OrderNum	OrderDate	CustName		Order_Total
+1102395		2018-04-04	Abernathy LLC		194.61
+1103850		2018-10-26	Abernathy LLC		5870.20
+1104205		2018-12-19	Abernathy LLC		191.11
+1102803		2018-06-04	Abra Lewis			24.99
+1104454		2019-01-25	Abra Lewis			108.00
+1104784		2019-03-14	Abra Lewis			54.00
+*/
